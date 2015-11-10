0615eaf1f045fdbf1b8b92ed1c4d5985b3bcd674
Tristan Camus <Kaendan@MacBook-Pro-de-Tristan.local>
2015-11-10 09:08:08 +0100

    Added the missing END of the module in ajaxview.

--- app/Tweelings/view/ajaxview.rb
+++ app/Tweelings/view/ajaxview.rb
@@ -10,23 +10,24 @@ module Tweelings
             CODE_SUCESS = 0
             CODE_NO_PARAMS = -1
 
-        def self.fetch_tweets(params)
-            # Check for params
-            if params == nil || params == "{}"
-                return JSON.generate({:code => CODE_NO_PARAMS})
-            end
+        	def self.fetch_tweets(params)
+            	# Check for params
+            	if params == nil || params == "{}"
+                	return JSON.generate({:code => CODE_NO_PARAMS})
+            	end
 
-            criteria = Criteria.new(params["theme"], params["number"])
-            result = Core.fetch_tweets(criteria)
-            puts "[AjaxView] fetch_tweets:: Response sent"
-            JSON.generate(:code => CODE_SUCCESS,
-                          :result => "#{result.count}")
-        end
+            	criteria = Criteria.new(params["theme"], params["number"])
+            	result = Core.fetch_tweets(criteria)
+            	puts "[AjaxView] fetch_tweets:: Response sent"
+            	JSON.generate(:code => CODE_SUCCESS,
+              	            :result => "#{result.count}")
+        	end
         
-        def self.save_tweets
-            result = Core.convert_tweets
-            JSON.generate(:code => CODE_SUCCESS,
-                          :result => "#{Utils.tweelings_to_a(result)}")
-        end
+	        def self.save_tweets
+	            result = Core.convert_tweets
+	            JSON.generate(:code => CODE_SUCCESS,
+	                          :result => "#{Utils.tweelings_to_a(result)}")
+	        end
+	    end
     end
 end
